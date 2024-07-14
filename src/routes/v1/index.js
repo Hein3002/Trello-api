d15a52a41741796dad70bdf327940606ee606346
Hein3002 <matcha24200@gmail.com>
2024-07-14 16:49:57 +0700

    Config Router

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,5 +1,15 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+import express from 'express'
+import { StatusCodes } from 'http-status-codes'
+import { boardRouters } from '~/routes/v1/boardRoutes'
+
+const Router = express.Router()
+
+//Check APIs V1 status
+Router.get('/status', (req, res) => {
+  res.status(StatusCodes.OK).json({ message: 'api v1' })
+})
+
+//Board APIs
+Router.use('/boards', boardRouters)
+
+export const APIs_V1 = Router
--- src/server.js
+++ src/server.js
@@ -3,20 +3,19 @@ import express from 'express'
 import exitHook from 'async-exit-hook'
 import { CONNECT_DB, CLOSE_DB } from './config/mongodb'
 import { env } from '~/config/environment'
+import { APIs_V1 } from '~/routes/v1'
 
 const START_SERVER = () => {
   const app = express()
 
-  app.get('/', async (req, res) => {
-    res.end('<h1>Hello World!</h1><hr>')
-  })
+  app.use('/v1', APIs_V1)
 
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     console.log(`Hello ${env.AUTHOR}, I am running at http://${env.APP_HOST}:${env.APP_PORT}/`)
   })
 
   //thuc hien dong connect khi dung server
-  exitHook(async() => {
+  exitHook(async () => {
     console.log('exit app')
     await CLOSE_DB()
   })
