e6f66fa1a89210a5bac09263aa5e52c5d168caee
Hein3002 <matcha24200@gmail.com>
2024-07-14 16:04:54 +0700

    Connect MongoDB and config env variables

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,5 +1,33 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+import { MongoClient, ServerApiVersion } from 'mongodb'
+import { env } from '~/config/environment'
+
+let trelloDatabaseInstance = null
+
+const mongoClientInstance = new MongoClient(env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
+
+//Ket noi database
+export const CONNECT_DB = async () => {
+  //goi ket noi toi mongodb atlas voi uri duoc khai bao trong than cua mongoClientInstance
+  await mongoClientInstance.connect()
+
+  //goi ket noi toi mongodb atlas voi uri duoc khai bao trong than cua mongoClientInstance
+  trelloDatabaseInstance = mongoClientInstance.db(env.DATABASE_NAME)
+}
+
+//Dong ket noi database
+export const CLOSE_DB = async () => {
+  await mongoClientInstance.close()
+}
+
+//export ra trelloDatabaseInstance sau khi da ket noi thanh cong de su dung lai o nhieu noi
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance) throw new Error('Must connect to Database first')
+  return trelloDatabaseInstance
+}
+
--- src/server.js
+++ src/server.js
@@ -1,32 +1,48 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
-
+/* eslint-disable no-console */
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import exitHook from 'async-exit-hook'
+import { CONNECT_DB, CLOSE_DB } from './config/mongodb'
+import { env } from '~/config/environment'
 
-const app = express()
+const START_SERVER = () => {
+  const app = express()
 
-const hostname = 'localhost'
-const port = 8017
+  app.get('/', async (req, res) => {
+    res.end('<h1>Hello World!</h1><hr>')
+  })
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(mapOrder(
-    [ { id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' } ],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    console.log(`Hello ${env.AUTHOR}, I am running at http://${env.APP_HOST}:${env.APP_PORT}/`)
+  })
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
-})
+  //thuc hien dong connect khi dung server
+  exitHook(async() => {
+    console.log('exit app')
+    await CLOSE_DB()
+  })
+}
+
+//Chi khi ket noi database thanh cong thi moi start server
+//ngoac tron thu 2 de thuc thi luon fuction (IIFE)
+(async () => {
+  try {
+    console.log('Connecting ...')
+    await CONNECT_DB()
+    console.log('Connected to mongodb Atlast')
+    START_SERVER()
+  } catch (error) {
+    console.error(error)
+    process.exit(0)
+  }
+})()
+
+// console.log('dang log khi chx xong connect')
+
+//Chi khi ket noi database thanh cong thi moi start server
+// CONNECT_DB()
+//   .then(() => console.log('Connected to mongodb Atlast'))
+//   .then(() => START_SERVER())
+//   .catch(error => {
+//     console.error(error)
+//     process.exit(0)
+//   })
