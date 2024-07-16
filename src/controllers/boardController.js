7266b4550e94c411115b2e5cf462fbfe38b6098c
Hein3002 <matcha24200@gmail.com>
2024-07-16 08:24:52 +0700

    Error Handling

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,15 +1,12 @@
 import { StatusCodes } from 'http-status-codes'
+import ApiError from '~/utils/ApiError'
 
 
 const createNew = async (req, res, next) => {
   try {
-    console.log('req.body:', req.body)
+    // console.log('req.body:', req.body)
     res.status(StatusCodes.CREATED).json({ message: 'POST from Controller create new Board' })
-  } catch (error) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      errors: error.message
-    })
-  }
+  } catch (error) { next(error) }
 
 }
 
--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -1,5 +1,6 @@
 import Joi from 'joi'
 import { StatusCodes } from 'http-status-codes'
+import ApiError from '~/utils/ApiError'
 
 
 const createNew = async (req, res, next) => {
@@ -24,10 +25,10 @@ const createNew = async (req, res, next) => {
     //Validate du hoan thien chi chuyen request sang Controller
     next()
   } catch (error) {
-    // console.log(error)
-    res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
-      errors: new Error(error).message
-    })
+    next(new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, new Error(error).message))
+    // res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
+    //   errors: new Error(error).message
+    // })
   }
 }
 
