270846e0c7d3328838b0f4117f9f5f9c752fe921
Hein3002 <matcha24200@gmail.com>
2024-07-16 21:39:58 +0700

    Service layer and create new Board

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,11 +1,14 @@
 import { StatusCodes } from 'http-status-codes'
-import ApiError from '~/utils/ApiError'
+import { boardService } from '~/services/boardService'
 
 
 const createNew = async (req, res, next) => {
   try {
     // console.log('req.body:', req.body)
-    res.status(StatusCodes.CREATED).json({ message: 'POST from Controller create new Board' })
+
+    const createBoard = await boardService.createNew(req.body)
+
+    res.status(StatusCodes.CREATED).json(createBoard)
   } catch (error) { next(error) }
 
 }
--- src/middlewares/errorHandlingMiddleware.js
+++ src/middlewares/errorHandlingMiddleware.js
@@ -13,8 +13,7 @@ export const errorHandlingMiddleware = (err, req, res, next) => {
   const responseError = {
     statusCode: err.statusCode,
     message: err.message || StatusCodes[err.statusCode], // Nếu lỗi mà không có message thì lấy ReasonPhrases chuẩn theo mã Status Code
-    stack: err.stack,
-    build_mode: process.env.BUILD_MODE
+    stack: err.stack
   }
   // console.error(responseError)
 
