b70a619c40e442667d8dc346190c92a9bd54ac28
Hein3002 <matcha24200@gmail.com>
2024-08-01 16:47:19 +0700

    Add WHITELIST_DOMAINS

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
 //Nhung domai dc phep truy cap den tai nguyen cua sever
 export const WHITELIST_DOMAINS = [
   // 'https://trello-api-au55.onrender.com'
+  'https://trello-web-puce.vercel.app/'
 ]
 
 export const BOARD_TYPE = {
